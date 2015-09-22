f01fbb8938352a252e0a57f5ee695ddd623e1945
Kevin Yeung <kyeung@thoughtworks.com>
2015-09-22 22:33:50 +0800

    Add makeCacheMatrix and cacheSolve for assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix sets up the "matrix" that can return the real matrix and its cached inverse, if any. It also has the ability to remember the computed inverse.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m = matrix()) {
+  original_matrix <<- m
+  inverse <- NULL
+  get_original <- function() original_matrix
+  set_inverse <- function(inv) inverse <<- inv
+  get_inverse <- function() inverse
+  list(get_original = get_original, set_inverse = set_inverse, get_inverse = get_inverse)
 }
 
 
-## Write a short comment describing this function
+## Ask the special "matrix" to give us the cached inverse. If there is none, ask for the original, real matrix and compute its inverse. The result is cached and returned.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(matrix, ...) {
+  cached <- matrix$get_inverse()
+  if(!is.null(cached)) {
+    message("returning cache")
+    return(cached)
+  }
+  message("computing inverse")
+  original <- matrix$get_original()
+  inverse <- solve(original)
+  matrix$set_inverse(inverse)
+  inverse
 }
